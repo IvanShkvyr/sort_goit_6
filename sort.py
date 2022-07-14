d55445d66b561215baa72756cb318ca02c7f9ab9
IvanShkvyr <Gis2011I@gmail.com>
2022-07-14 11:01:11 +0300

    redesigned function "main" and created function "check_path" and "iter_files_on_dirs"

--- sort.py
+++ sort.py
@@ -1,11 +1,7 @@
-import re
+import pathlib
 import sys
 
 
-
-CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
 TRANS = {
         1072: 'a', 1040: 'A', 1073: 'b', 1041: 'B', 1074: 'v', 1042: 'V', 1075: 'g', 1043: 'G', 1076: 'd', 1044: 'D',
         1077: 'e', 1045: 'E', 1105: 'e', 1025: 'E', 1078: 'j', 1046: 'J', 1079: 'z', 1047: 'Z', 1080: 'i', 1048: 'I',
@@ -18,8 +14,58 @@ TRANS = {
         }
 
 
-def main():  # бажано щоб ця функція не отримувала ніяких аргументів
-    return sys.argv[1]
+def main():
+
+    """
+    the main function
+    """
+    dir_list, file_list = [], []
+
+    if len(sys.argv) < 2:
+        user_path = ""
+    else:
+        user_path = sys.argv[1]
+
+    path = check_path(user_path)
+
+    dir_list, file_list = iter_files_on_dirs(path, dir_list, file_list)
+
+
+
+
+
+
+
+def check_path(user_path):
+    
+    """
+    checks the path to the file folder
+    """
+    path = pathlib.Path(user_path)
+    if path.exists():
+        if path.is_dir():
+            return path
+        else:
+            print(f"{path} is file")
+    else:
+        print(f"path {path.absolute()} not exists")
+
+
+def iter_files_on_dirs(path, dir_list, file_list):
+    
+    """
+    the function iterates files and folders through the parent folder
+    returns a list of file addresses and a list of folder addresses
+    """
+    if path.is_dir():
+
+        dir_list.append(path.absolute())
+        for element in path.iterdir():
+            iter_files_on_dirs(element, dir_list, file_list)
+    else:
+        file_list.append(path.absolute())
+    
+    return dir_list, file_list
 
 
 def normalize(file_name):
@@ -38,6 +84,9 @@ def normalize(file_name):
 
 
 if __name__ == "__main__":
-    print(main())
+    main()
 
 #python sort.py C:\Users\user\Desktop\Rozibraty
+#python sort.py C:\Users\user\Desktop\Rozibraty\відео_файл.avi
+#python sort.py grfeffde
+#python sort.py
