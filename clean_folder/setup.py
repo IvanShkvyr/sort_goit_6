54df4554ba494425c22c439e6ab010f1d4cf27b2
IvanShkvyr <Gis2011I@gmail.com>
2022-07-20 22:41:26 +0300

    preparation console script

---
+++ clean_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='clean_foldef',
+      version='1.0.0',
+      description='Сode that sorts files by category',
+      url='https://github.com/IvanShkvyr/sort_goit_6',
+      author='Ivan Shkvyr',
+      author_email='GIS2011i@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      entry_points={'console_scripts': ['sort=clean_foldef.sort:main']}
+      )
